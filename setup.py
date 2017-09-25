19d649718faaab5b3851d3cd2217436cbd8d013c
Cyrill Gorcunov <gorcunov@gmail.com>
2017-09-25 11:19:34 +0300

    gScope: v0.2
     - Fix dup'ing of cource code window when same file
       opened via File/Open menu and Cscope reference
     - Add "ui" section in json config
     - When opening Vim editor place cursor at the same
       position as it was in source view
     - Show filter in file opening dialog
     - Fix notebook activations
     - Fix tags accumulation
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(name = 'gscope',
-      version = '0.1',
+      version = '0.2',
       description = 'Simple GTK forntend for cscope',
       author = 'Cyrill Gorcunov',
       author_email = 'gorcunov@gmail.com',
       url = 'https://github.com/cyrillos/gscope',
-      download_url = 'https://github.com/cyrillos/gscope/archive/v0.1.tar.gz',
+      download_url = 'https://github.com/cyrillos/gscope/archive/v0.2.tar.gz',
       packages = ['gscope'],
       package_dir = {'gscope' : 'src'},
       package_data = {'gscope': ['data/*']},
