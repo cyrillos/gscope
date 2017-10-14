afeeab0ebb7f3d3a24f04c15215120797299947d
Cyrill Gorcunov <gorcunov@gmail.com>
2017-10-14 12:42:10 +0300

    gScope: v0.6
     * Add --proj and --file arguments for CLI
     * Allow to open multiple source files at once
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name = 'gscope',
-      version = '0.5',
+      version = '0.6',
       description = 'Simple GTK forntend for cscope',
       author = 'Cyrill Gorcunov',
       author_email = 'gorcunov@gmail.com',
